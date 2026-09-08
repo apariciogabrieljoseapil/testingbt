1ad0e7ec12da8ab836f8d9b930049c9c8b11a39a
apariciogabrieljoseapil <apariciogabrieljose@gmail.com>
2026-09-08 14:20:41 +0800

    Inital

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import { createClient } from '@supabase/supabase-js';
 import cookieParser from 'cookie-parser';
+const multer = require('multer');
 import jwt from 'jsonwebtoken';
 const app = express();
 
